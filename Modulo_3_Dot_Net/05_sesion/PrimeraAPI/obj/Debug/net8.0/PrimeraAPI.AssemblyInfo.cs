2ae0f2037bfd117c43b7b9ae4e14f7b2bc01ad89
vzaldivarg <victor.zaldivar@megacable.com.mx>
2025-05-16 12:48:50 -0600

    Ok

--- Modulo_3_Dot_Net/05_sesion/PrimeraAPI/obj/Debug/net8.0/PrimeraAPI.AssemblyInfo.cs
+++ Modulo_3_Dot_Net/05_sesion/PrimeraAPI/obj/Debug/net8.0/PrimeraAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a45de83a97f4baf422b7846e58026e38e5239e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb71bcc7d95cc6e17ac702f6fc6d89e6d1da6bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
