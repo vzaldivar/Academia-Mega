5da1e4e27114720594dc454978ee71b3dd8bd0bc
vzaldivarg <victor.zaldivar@megacable.com.mx>
2025-05-27 15:52:43 -0600

    Dia11.2

--- Modulo_3_Dot_Net/07_sesion/TiendaMVC/obj/Debug/net8.0/TiendaMVC.AssemblyInfo.cs
+++ Modulo_3_Dot_Net/07_sesion/TiendaMVC/obj/Debug/net8.0/TiendaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90edb33fb5f0aabc291d300425654e54553fa98f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05dbe543d121c94d5471b968583d10df8eba083")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
