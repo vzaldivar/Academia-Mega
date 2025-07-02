7c78859e2744b04b7cd4254178f65df46e78a09e
vzaldivar <victor.zaldivar@megacable.com.mx>
2025-07-02 10:55:49 -0600

    010725

--- Modulo_3_Dot_Net/08_sesion/ChatClient/obj/Debug/net8.0/ChatClient.AssemblyInfo.cs
+++ Modulo_3_Dot_Net/08_sesion/ChatClient/obj/Debug/net8.0/ChatClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3776af205e275170a2ea32679ee83691181dc3be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67004541cbe78776d157ccf179c0772caf05abe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
