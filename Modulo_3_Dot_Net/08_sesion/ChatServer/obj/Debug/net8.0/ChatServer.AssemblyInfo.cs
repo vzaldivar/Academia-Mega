f5455de2a9baf468d76506deb571d854bef2d5f5
vzaldivar <victor.zaldivar@megacable.com.mx>
2025-07-02 10:11:51 -0600

    Ok

--- Modulo_3_Dot_Net/08_sesion/ChatServer/obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
+++ Modulo_3_Dot_Net/08_sesion/ChatServer/obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9f7039ae679628c3ab4c8bfa78003cfadd5294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a129d12e9547b8b097bd78ba830d8951ff4f51e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
